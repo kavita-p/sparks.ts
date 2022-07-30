c973241a876d695e5eb97c206863674d4447da92
Kavita <kavi.pdri@gmail.com>
2022-07-29 18:13:48 -0700

    if else if test

--- index.js
+++ index.js
@@ -7,4 +7,22 @@ client.once('ready', () => {
   console.log('Sparks, ready!');
 });
 
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName == 'buzz') {
+    await interaction.reply('Zap!');
+  } else if (commandName === 'server') {
+    await interaction.reply(
+      `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
+    );
+  } else if (commandName === 'user') {
+    await interaction.reply(
+      `Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`
+    );
+  }
+});
+
 client.login(token);
