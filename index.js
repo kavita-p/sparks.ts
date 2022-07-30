906e86f22ba6bf30f8e51077fd151d6ff3580157
Kavita <kavi.pdri@gmail.com>
2022-07-29 18:07:56 -0700

    prettier singlequotes

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const { Client, GatewayIntentBits } = require("discord.js");
-const { token } = require("./config.json");
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once("ready", () => {
-  console.log("Sparks, ready!");
+client.once('ready', () => {
+  console.log('Sparks, ready!');
 });
 
 client.login(token);
