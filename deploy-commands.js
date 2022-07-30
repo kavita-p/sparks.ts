41baa83af1a12d9200af1eca3799654bf5578807
Kavita <kavi.pdri@gmail.com>
2022-07-29 17:57:36 -0700

    deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder, Routes } = require('disord.js');
+const { REST } = require("@discordjs/rest");
+const { clientId, guildId, token } = require("config.json");
+
+const commands = [
+  new SlashCommandBuilder().setName('buzz').setDescription('Replies with zap!')
+  new SlashCommandBuilder().setName('server').setDescription('Replies with server info.')
+  new SlashCommandBuilder().setName('user').setDescription('Replies with user info.')
+]
+  .map(command => command.toJSON())
+
+const rest = new REST({version: '10'}).setToken(token)
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+  .then(()=> console.log('Succesfully registered application commands.'))
+  .catch(console.error)
