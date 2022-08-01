20c433cae88f756c33ab8a4f21f3eba96bc7e73c
Kavita <kavi.pdri@gmail.com>
2022-08-01 15:54:02 -0700

    interactionCreate looks pretty now

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,21 +1,24 @@
-module.exports = {
-  name: 'interactionCreate',
-  async execute(commands, interaction) {
-    console.log(`${interaction.user.tag} triggered an interaction.`);
-    if (!interaction.isChatInputCommand()) return;
+import { Collection, Interaction } from 'discord.js';
 
-    const command = commands.get(interaction.commandName);
+export const name = 'interactionCreate';
+export const execute = async (
+  commands: Collection<any, any>,
+  interaction: Interaction
+) => {
+  console.log(`${interaction.user.tag} triggered an interaction.`);
+  if (!interaction.isChatInputCommand()) return;
 
-    if (!command) return;
+  const command = commands.get(interaction.commandName);
 
-    try {
-      await command.execute(interaction);
-    } catch (error) {
-      console.error(error);
-      await interaction.reply({
-        content: 'There was an error while executing this command!',
-        ephemeral: true,
-      });
-    }
-  },
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: 'There was an error while executing this command!',
+      ephemeral: true,
+    });
+  }
 };
