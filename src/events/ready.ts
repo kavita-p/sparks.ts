b4c98f40f15d3a85d119ca7034db90380701a5b7
Kavita <kavi.pdri@gmail.com>
2022-08-01 14:10:12 -0700

    migrated ready event to ts successfully

---
+++ src/events/ready.ts
@@ -0,0 +1,7 @@
+import { Client } from 'discord.js';
+
+export const name = 'ready';
+export const once = true;
+export const execute = (client: Client) => {
+  console.log(`Sparks, ready! Logged in as ${client.user.tag}.`);
+};
--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ for (const file of commandFiles) {
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs
   .readdirSync(eventsPath)
-  .filter((file) => file.endsWith('.js'));
+  .filter((file) => file.endsWith('.ts'));
 
 for (const file of eventFiles) {
   const filePath = path.join(eventsPath, file);
