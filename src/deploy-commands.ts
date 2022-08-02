9ac384b4d79a75cab6b04dc8637a54de3ffb1e62
Kavita <kavi.pdri@gmail.com>
2022-08-01 17:14:21 -0700

    update deploy script

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -8,7 +8,7 @@ const commands = [];
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs
   .readdirSync(commandsPath)
-  .filter((file) => file.endsWith('.js'));
+  .filter((file) => file.endsWith('.ts'));
 
 for (const file of commandFiles) {
   const filePath = path.join(commandsPath, file);
